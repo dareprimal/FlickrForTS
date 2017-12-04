0b6cdfc83e62143a3ce235c0ba91231711a88307
dareprimal <idonthavefriends@gmail.com>
2017-12-04 19:38:06 +0100

    Add drawables icons for: - tags - publish date - empty image

--- app/src/main/java/com/demoforts/android/flickrpublicfeed/ui/publicfeed/view/PublicFeedActivity.kt
+++ app/src/main/java/com/demoforts/android/flickrpublicfeed/ui/publicfeed/view/PublicFeedActivity.kt
@@ -1,7 +1,7 @@
 package com.demoforts.android.flickrpublicfeed.ui.publicfeed.view
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.demoforts.android.flickrpublicfeed.R
 
 class PublicFeedActivity : AppCompatActivity() {
