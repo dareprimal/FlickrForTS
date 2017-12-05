d1b1087713cca824aa0dbf1b1d558acc46531ec1
dareprimal <idonthavefriends@gmail.com>
2017-12-05 08:34:17 +0100

    Retrofit added to project

---
+++ app/src/main/java/com/demoforts/android/flickrpublicfeed/data/publicfeed/network/NetworkConnectionService.kt
@@ -0,0 +1,43 @@
+package com.demoforts.android.flickrpublicfeed.data.publicfeed.network
+
+import com.demoforts.android.flickrpublicfeed.BuildConfig
+import com.demoforts.android.flickrpublicfeed.data.publicfeed.protocol.GsonConverter
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by Michal S. on 05.12.2017.
+ */
+object NetworkConnectionService {
+    private val httpClient: OkHttpClient = getOkHttpClient()
+    private val connectionTimeout: Long = 1
+    private val writeTimeout: Long = 1
+    private val readTimeout: Long = 1
+
+
+    fun <S> generateService(
+            serviceClass: Class<S>): S {
+
+        return Retrofit.Builder()
+                .client(httpClient)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverter.converter)
+                .baseUrl(BuildConfig.API_URL)
+                .build()
+                .create(serviceClass)
+    }
+
+    private fun getOkHttpClient(): OkHttpClient {
+        val httpClientBuilder = OkHttpClient.Builder()
+                .connectTimeout(connectionTimeout, TimeUnit.MINUTES)
+                .writeTimeout(writeTimeout, TimeUnit.MINUTES)
+                .readTimeout(readTimeout, TimeUnit.MINUTES)
+        if (BuildConfig.DEBUG) {
+            httpClientBuilder.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        }
+        return httpClientBuilder.build()
+    }
+}
---
+++ app/src/main/java/com/demoforts/android/flickrpublicfeed/data/publicfeed/protocol/GsonConverter.kt
@@ -0,0 +1,18 @@
+package com.demoforts.android.flickrpublicfeed.data.publicfeed.protocol
+
+import com.google.gson.GsonBuilder
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by Michal S. on 05.12.2017.
+ */
+object GsonConverter {
+
+    private var gsonConverter: GsonConverterFactory =
+            GsonConverterFactory.create(GsonBuilder().create())
+
+    val converter: GsonConverterFactory
+        get() {
+            return gsonConverter
+        }
+}
