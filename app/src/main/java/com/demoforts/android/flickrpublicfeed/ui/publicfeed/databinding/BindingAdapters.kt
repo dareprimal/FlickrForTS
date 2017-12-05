4368de97b089e81459e652de018c26c0354be459
dareprimal <idonthavefriends@gmail.com>
2017-12-05 13:33:14 +0100

    Databinding for images: - add load image databinding - add picasso to project dependences

--- app/src/main/java/com/demoforts/android/flickrpublicfeed/ui/publicfeed/databinding/BindingAdapters.kt
+++ app/src/main/java/com/demoforts/android/flickrpublicfeed/ui/publicfeed/databinding/BindingAdapters.kt
@@ -2,8 +2,10 @@ package com.demoforts.android.flickrpublicfeed.ui.publicfeed.databinding
 
 import android.databinding.BindingAdapter
 import android.support.v7.widget.RecyclerView
+import android.widget.ImageView
 import com.demoforts.android.flickrpublicfeed.domain.publicfeed.PublicFeedItem
 import com.demoforts.android.flickrpublicfeed.ui.publicfeed.view.adapter.PublicFeedListAdapter
+import com.squareup.picasso.Picasso
 
 /**
  * Created by Michal S. on 05.12.2017.
@@ -15,4 +17,9 @@ fun bindList(recyclerView: RecyclerView, items: List<PublicFeedItem>?) {
     } else {
         recyclerView.adapter = PublicFeedListAdapter(items, recyclerView.context)
     }
+}
+
+@BindingAdapter("imageBind")
+fun bindFlickrImage(imageView: ImageView, imageUrl: String) {
+    Picasso.with(imageView.context).load(imageUrl).into(imageView)
 }
